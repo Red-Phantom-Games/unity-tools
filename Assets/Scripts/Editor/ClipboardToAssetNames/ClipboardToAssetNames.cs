f7d23491a60c58cc18c381fd0c8829249552b222
Richard Ogden <richard_ogden@redphantomgames.com>
2020-04-20 15:02:28 +0100

    Add files via upload

---
+++ Assets/Scripts/Editor/ClipboardToAssetNames/ClipboardToAssetNames.cs
@@ -0,0 +1,66 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityTools
+{
+    public class ClipboardToAssetNames : MonoBehaviour
+    {
+        //---------------------------------------------------------------------------------
+
+        [MenuItem( "Assets/Paste Names", true )]
+        private static bool ClipboardToAssetNames_Validate()
+        {
+            // Get list of selected assets in project window
+            Object[] objects = Selection.GetFiltered<Object>( SelectionMode.Assets );
+
+            // We allow the paste if there's at least one asset selected and the clipboard has some string in it
+            return (objects.Length != 0 && GUIUtility.systemCopyBuffer != "");
+        }
+
+        //---------------------------------------------------------------------------------
+
+        [MenuItem( "Assets/Paste Names", false )]
+        private static void ClipboardToAssetNames_Do()
+        {
+            // Convert clipboard into an array of strings to be used as asset names
+            string[] filter = { System.Environment.NewLine };
+            string[] entries = GUIUtility.systemCopyBuffer.Split( filter, System.StringSplitOptions.RemoveEmptyEntries );
+
+            // Get list of selected assets in project window
+            Object[] objects = Selection.GetFiltered<Object>( SelectionMode.Assets );
+
+            bool proceedWithRenaming = true;
+
+            // Throw a dialog warning if the number of selected assets isn't the same as what we've got from the clipboard
+            if( objects.Length != entries.Length )
+            {
+                proceedWithRenaming = EditorUtility.DisplayDialog( "Renaming Assets From Clipboard Warning!", "Number of clipboard entires differ from number of selected assets. Proceed?", "Continue", "Cancel" );
+            }
+
+            // Continue with renaming the assets if allowed
+            if( proceedWithRenaming )
+            {
+                int entryIndex = 0;
+
+                foreach( Object obj in objects )
+                {
+                    // Quit if we run out of names from the clipboard
+                    if( entryIndex >= entries.Length )
+                    {
+                        break;
+                    }
+
+                    // Assuming asset path of a selected object is always valid
+                    string assetPath = AssetDatabase.GetAssetPath( obj );
+
+                    // Rename asset to name from from clipboard
+                    AssetDatabase.RenameAsset( assetPath, entries[entryIndex] );
+                    ++entryIndex;
+                }
+            }
+        }
+
+        //---------------------------------------------------------------------------------
+    }
+}
---
+++ Assets/Scripts/Editor/SceneSelectDropdown/SceneSelectDropdown.cs
@@ -0,0 +1,103 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace UnityTools
+{
+    public class SceneSelectDropdown : EditorWindow
+    {
+        private bool m_showOnlyScenesInBuild = false;
+
+        //---------------------------------------------------------------------------------
+
+        [MenuItem("Unity Tools/Scene Select Dropdown")]
+        public static void Init()
+        {
+            SceneSelectDropdown window = EditorWindow.GetWindow<SceneSelectDropdown>( "Scene Select Dropdown" );
+            window.minSize = new Vector2( 50.0f, 15.0f );
+            window.Show();
+        }
+
+        //---------------------------------------------------------------------------------
+
+        protected virtual void OnGUI()
+        {
+            int current = -1;
+
+            string[] scenePaths;
+
+            if( m_showOnlyScenesInBuild )
+            {
+                scenePaths = GetScenePaths_AddedToBuild();
+            }
+            else
+            {
+                scenePaths = GetScenePaths_AllInProject();
+            }
+
+            string[] sceneNames = new string[scenePaths.Length];
+
+            for( int i = 0; i < scenePaths.Length; ++i )
+            {
+                // Work out the scene name from its path
+                int lastSlash = scenePaths[i].LastIndexOf( "/" );
+                string name = scenePaths[i].Substring( lastSlash + 1 );
+                name = name.Replace( ".unity", "" );
+
+                sceneNames[i] = name;
+
+                if( EditorSceneManager.GetActiveScene().name == name )
+                {
+                    current = i;
+                }
+            }
+
+            int newSceneIndex = EditorGUILayout.Popup( current, sceneNames );
+
+            if( newSceneIndex != current )
+            {
+                bool save = EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+                if( save )
+                {
+                    EditorSceneManager.OpenScene( scenePaths[newSceneIndex], OpenSceneMode.Single );
+                }
+            }
+
+            m_showOnlyScenesInBuild = EditorGUILayout.Toggle( "Scenes In Build Only", m_showOnlyScenesInBuild );
+        }
+
+        //---------------------------------------------------------------------------------
+
+        private string[] GetScenePaths_AllInProject()
+        {
+            string[] sceneAssets = AssetDatabase.FindAssets("t:Scene");
+
+            string[] scenePaths = new string[sceneAssets.Length];
+
+            for( int i = 0; i < sceneAssets.Length; ++i )
+            {
+                scenePaths[i] = AssetDatabase.GUIDToAssetPath( sceneAssets[i] );
+            }
+
+            return scenePaths;
+        }
+
+        //---------------------------------------------------------------------------------
+
+        private string[] GetScenePaths_AddedToBuild()
+        {
+            string[] scenePaths = new string[EditorBuildSettings.scenes.Length];
+
+            for( int i = 0; i < EditorBuildSettings.scenes.Length; ++i )
+            {
+                scenePaths[i] = EditorBuildSettings.scenes[i].path;
+            }
+
+            return scenePaths;
+        }
+
+        //---------------------------------------------------------------------------------
+    }
+}
